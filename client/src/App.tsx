377b8b799cc2048d7f4984142958c88cd670b600
Matheus Rodrigues Araujo <matheus.rodgs.araujo13@gmail.com>
2023-09-02 16:10:52 -0300

    tailwind has been installed and configured

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,15 @@
 // import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
-import './App.css'
+// import './App.css'
 
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
-    <>
-      <h1>React + Vite app</h1>
-    </>
+   <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
   )
 }
 
