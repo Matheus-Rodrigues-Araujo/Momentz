c502e4f3eabdd1cd76d3c5c45a7066b6bb07635e
Matheus Rodrigues Araujo <matheus.rodgs.araujo13@gmail.com>
2023-09-08 20:52:13 -0300

    NEW: Register page updated

--- client/src/app/register/page.tsx
+++ client/src/app/register/page.tsx
@@ -20,7 +20,7 @@ const Register = () => {
                 <label className="block text-gray-700 text-sm font-bold mb-2">
                     Email
                 </label>
-                <input className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="email" type="email" placeholder="Email" />
+                <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="email" type="email" placeholder="Email" />
                 {/* <p className="text-red-500 text-xs italic">Please choose a password.</p> */}
             </div>
             <div className="mb-4">
