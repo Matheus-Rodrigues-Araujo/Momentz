0880702dcafe4371114a9b3bd5e87e2b13334bae
Matheus Rodrigues Araujo <68081476+Matheus-Rodrigues-Araujo@users.noreply.github.com>
2023-10-06 21:19:11 -0300

    Server folder was removed | Name from package.json has been changed to 'app'

--- client/db.js
+++ client/db.js
@@ -1,21 +1,3 @@
-// import mongoose from 'mongoose';
-
-// const uri = process.env.DATABASE_URI
-// const DB_URI = uri;
-
-// const connectDB = async () => {
-//   try {
-//     await mongoose.connect(MONGODB_URI, {
-//       useNewUrlParser: true,
-//       useUnifiedTopology: true,
-//     });
-//     console.log('Conectado ao MongoDB');
-//   } catch (error) {
-//     console.error('Erro ao conectar ao MongoDB:', error);
-//   }
-// };
-
-// export default connectDB;
 import { MongoClient } from 'mongodb';
 const uri = process.env.DATABASE_URI
 
