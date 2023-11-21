d01f49843f0615dd877222d7ceaac94e1c986dce
Matheus Rodrigues Araujo <68081476+Matheus-Rodrigues-Araujo@users.noreply.github.com>
2023-11-21 12:58:19 -0300

    tailwind.config: plugin configured

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -27,17 +27,6 @@ const config: Config = {
       },
     },
   },
-  plugins: [
-    ({ addUtilities }:any) => {
-      const newUtilities = {
-        '.clip-custom': {
-          position: 'absolute',
-          clip: 'rect(0px, 65px, 200px, 0px)',
-          top: '1.25em',
-        },
-      };
-      addUtilities(newUtilities, ['responsive', 'hover']);
-    },
-  ],
+  plugins: [],
 }
 export default config
