eb900769ff7303781cbcf8bc7d6359dc6ceb5d52
Matheus Rodrigues Araujo <68081476+Matheus-Rodrigues-Araujo@users.noreply.github.com>
2023-11-21 15:36:05 -0300

    Sidebar Component responsiveness for mobile devices has been configured

--- client/src/components/sidebar.tsx
+++ client/src/components/sidebar.tsx
@@ -14,9 +14,7 @@ import {
 import { useEffect, useState } from "react"
 
 export default function Sidebar(){
-  const [windowWidth, setWindowWidth] = useState(()=> {
-    return typeof window !== 'undefined' ? window.innerWidth : 0
-  })  
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth || 0)  
   
   useEffect(()=> {
     window.addEventListener('resize', () => setWindowWidth(window.innerWidth))
@@ -24,16 +22,16 @@ export default function Sidebar(){
   }, [windowWidth])
 
   return(
-        <div className="bg-customDark text-white px-3 fixed grid justify-center md:bg-customDark h-screen top-0 left-0 border-r">  
-          <div className="mt-5 ml-2">
-            <Link href="/" className="" style={{marginBottom: '80px'}} >
+        <div className="bg-customDark w-full text-white px-3 fixed bottom-0 md:top-0 md:w-auto md:border-r md:left-0">  
+          <div className="flex flex-row justify-center items-center mt-0 ml-0 md:block md:mt-5 md:ml-1">
+            <Link href="/" className="mb-0 md:mb-[80px]">
               <div className="flex items-center text-center">
                 {windowWidth >= 1280 ? <Image src={logo} width={180} height={100}   alt="Logo"/>
-                : <Image src={logoClip} width={60} height={50} alt="Logo"/>}
+                : <Image src={logoClip} className=" w-[50px]" alt="Logo"/>}
               </div>
             </Link>
             <Link href="/" className="nav-item-container" >
-              <div className="flex gap-4 items-center p-2 text-center mt-10">
+              <div className="sm:mt-0 md:mt-10 flex gap-4 items-center p-2 text-center">
                 <div className="icon-container bg-white p-2 bg-white rounded-full " >
                   <HomeIcon className="h-6 w-6 text-customDark  text-black" />
                 </div>
