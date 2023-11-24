49dca0da6953be864b3d397a37908e917d4ecbe2
Matheus Rodrigues Araujo <68081476+Matheus-Rodrigues-Araujo@users.noreply.github.com>
2023-11-24 01:00:26 -0300

    page.tsx responsiveness has been updated

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -4,10 +4,10 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex min-h-[500px] max-w-[500px] my-10 mx-auto flex-col justify-center px-6 py-12 rounded-md border border-customYellow lg:px-8">
+    <div className="sm:flex w-[260px] my-10 mx-auto flex-col justify-center px-2 py-8 rounded-md border border-customYellow md:w-[500px] px-6 py-12 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-sm">
         <Link href={'/'}>
-          <Image src={Logo} alt="Your Company" />
+          <Image src={Logo} alt="Your Company" className="w-auto h-50"  />
         </Link>
       </div>
 
@@ -22,8 +22,10 @@ export default function Home() {
                 type="email"
                 autoComplete="email"
                 required
-                className="form-input block w-full rounded-md border-0 p-5 text-white shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-yellow-100 sm:text-sm sm:leading-6"
-                style={{ color: 'white' }}
+                className="
+                form-input block w-full rounded-md border-0 p-3 text-white
+                shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400
+                focus:ring-2 focus:ring-inset focus:ring-yellow-100 sm:text-sm sm:leading-6 md:p-5"
               />
             </div>
           </div>
@@ -39,8 +41,10 @@ export default function Home() {
                 type="password"
                 autoComplete="current-password"
                 required
-                className="form-input block w-full rounded-md border-0 p-5 text-white shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-yellow-100 sm:text-sm sm:leading-6"
-                style={{ color: 'white' }}
+                className="
+                form-input block w-full rounded-md border-0 p-3 text-white
+                shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400
+                focus:ring-2 focus:ring-inset focus:ring-yellow-100 sm:text-sm sm:leading-6 md:p-5"
               />
             </div>
           </div>
@@ -48,21 +52,25 @@ export default function Home() {
           <div>
             <button
               type="submit"
-              className="sign-btn flex w-full bg-customLightbrown justify-center rounded-md p-5 text-sm font-semibold uppercase leading-6 text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              className="
+              sign-btn flex w-full bg-customLightbrown justify-center
+              rounded-md p-3 text-sm font-semibold uppercase leading-6
+              text-white shadow-sm focus-visible:outline focus-visible:outline-2
+              focus-visible:outline-offset-2 focus-visible:outline-indigo-600 md:p-5 "
             >
               Sign in
             </button>
           </div>
         </form>
 
-        <p className="mt-10 text-center text-md text-gray-400">
+        <p className="text-sm mt-10 text-center text-gray-400 md:text-md">
           Don't have an account?
-          <Link href='/register' className="font-semibold leading-6 text-indigo-600 text-customYellow hover:text-yellow-100 underline">Create here</Link>
+          <Link href='/register' className="font-semibold leading-6 text-sm text-indigo-600 text-customYellow hover:text-yellow-100 underline md:text-md">Create here</Link>
 
         </p>
-        <p className="mt-10 text-center text-md text-gray-400">
+        <p className="text-sm mt-10 text-center text-gray-400 md:text-md">
           Forgot password?
-          <Link href='/account/password' className="font-semibold leading-6 text-indigo-600 text-customYellow hover:text-yellow-100 underline">Click here</Link>
+          <Link href='/account/password' className="font-semibold leading-6 text-sm text-indigo-600 text-customYellow hover:text-yellow-100 underline md:text-md">Click here</Link>
         </p>
       </div>
     </div>
