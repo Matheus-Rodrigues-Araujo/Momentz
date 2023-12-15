f92c75599eddbb50ede576046700621b72b2bc48
Matheus Rodrigues Araujo <68081476+Matheus-Rodrigues-Araujo@users.noreply.github.com>
2023-12-15 18:00:49 -0300

    updated

--- client/src/components/sidebar.tsx
+++ client/src/components/sidebar.tsx
@@ -74,7 +74,7 @@ export default function Sidebar({userData}: {userData: UserData | null}){
                 <p className="hidden xl:block">Messages</p>
               </div>
             </Link>
-            <Link href="/" className="nav-item-container">
+            <Link href="/next/post" className="nav-item-container">
               <div className="flex gap-4 items-center p-2 text-center">
                 <div className="icon-container bg-white p-2 bg-white rounded-full" >
                   <PlusIcon className=" h-6 w-6 text-customDark  text-black" />
