bd441ddc5ab5b597d21a6b36cbe78a57f978bf75
Matheus Rodrigues Araujo <68081476+Matheus-Rodrigues-Araujo@users.noreply.github.com>
2023-12-27 12:45:50 -0300

    API Rest has been updated: Comment Requests

--- client/src/app/api/auth/comment/[id]/route.ts
+++ client/src/app/api/auth/comment/[id]/route.ts
@@ -2,22 +2,64 @@ import { NextRequest, NextResponse } from "next/server";
 import Comment from "@/app/models/comment";
 import { IComment } from "@/app/models/comment";
 
+export async function GET(
+  req: NextRequest,
+  context: { params: { id: string } }
+) {
+  try {
+    const postId = context.params.id;
+    const comment = await Comment.findOne({ postId: postId });
+
+    return NextResponse.json(comment, { status: 200 });
+  } catch (error) {
+    return NextResponse.json(
+      { message: "Error receiving comment", error },
+      { status: 500 }
+    );
+  }
+}
+
 export async function POST(
   req: NextRequest,
   context: { params: { id: string } }
 ) {
-  try{
-  const postId = context.params.id;
-  const { userId, content } = await req.json();
- 
-  const comments:IComment = await Comment.create({
-    authorId: userId,
-    postId: postId,
-    content: content,
-  });
-  return NextResponse.json({ message: "Comment created"}, {status: 200});
+  try {
+    const postId = context.params.id;
+    const { userId, content } = await req.json();
+
+    const comments: IComment = await Comment.create({
+      authorId: userId,
+      postId: postId,
+      content: content,
+    });
+    return NextResponse.json({ message: "Comment created" }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json(
+      { message: "Error creating comment", error },
+      { status: 500 }
+    );
   }
-  catch(error){
-    return NextResponse.json({ message: "Error creating comment", error}, {status: 500});
+}
+
+export async function PUT(
+  req: NextRequest,
+  context: { params: { id: string } }
+) {
+  try {
+    const commentId = context.params.id;
+    const { userId, newContent } = await req.json();
+    const comment = await Comment.findOne({ _id: commentId });
+    if (userId !== String(comment.authorId)) {
+      return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+    }
+
+    await Comment.findByIdAndUpdate(commentId, {
+      $set: { content: newContent },
+    });
+    const newComment = await Comment.findById(commentId);
+
+    return NextResponse.json({ message: "Comment updated" }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ message: "Error to update" }, { status: 500 });
   }
 }
